b074839f51c27dc04411a4b65342b4048b61e157
bcharishmareddy333-prog <bcharishmareddy333@gmail.com>
2026-07-08 21:57:04 +0530

    Day3

---
+++ Day3/Divisible35.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Divisible35{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number:");
+        int num=sc.nextInt();
+        if(num%3==0&&num%5==0){
+            System.out.println("it is divisible");
+        }else{
+            System.out.println("it is not divisible");
+        }
+    }
+}
